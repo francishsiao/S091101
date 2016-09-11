e5dd23cc91e8c147ec9906048f33f1a03f96ff45
student <fyhsiao0101@gmail.com>
2016-09-11 14:32:53 +0800

    無關緊要

--- app/src/main/java/com/example/student/s091101/MapsActivity.java
+++ app/src/main/java/com/example/student/s091101/MapsActivity.java
@@ -67,8 +67,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
         // Add a marker in Sydney and move the camera
-        LatLng taipei = new LatLng(25.0162, 121.533);
-        mMap.addMarker(new MarkerOptions().position(taipei).title("Marker in Taipei").icon(BitmapDescriptorFactory.fromResource(R.drawable.byke)));
+        LatLng taipei = new LatLng(24.9834668,121.4525423);
+        mMap.addMarker(new MarkerOptions().position(taipei).title("Marker in Taipei").icon(BitmapDescriptorFactory.fromResource(R.drawable.cat)));
 
         //新增位置標示
         MarkerOptions mo1 = new MarkerOptions().position(new LatLng(25.234, 121.5)).title("Test"); //Simple factory pattern
